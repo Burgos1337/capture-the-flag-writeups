679cb125b23e30516b49e56c852c2ba7600ededb
Dmitriy Rodionov <57829161+Burgos1337@users.noreply.github.com>
2020-08-21 15:05:13 +0300

    some writeups added

--- 2020/DawgCTF 2020/Left Foot Two Stomps/solution/rsa_decode.py
+++ 2020/DawgCTF 2020/Left Foot Two Stomps/solution/rsa_decode.py
@@ -1,16 +1,16 @@
 def egcd(a, b):
     if a == 0:
-        return (b, 0, 1)
+       return (b, 0, 1)
     else:
-        g, y, x = egcd(b % a, a)
-        return (g, x - (b // a) * y, y)
+       g, y, x = egcd(b % a, a)
+       return (g, x - (b // a) * y, y)
 
 def modinv(a, m):
     g, x, y = egcd(a, m)
     if g != 1:
-        raise Exception("Modular inverse doesn't exist")
+       raise Exception("Modular inverse doesn't exist")
     else:
-        return x % m
+       return x % m
 
 output = ""
 
