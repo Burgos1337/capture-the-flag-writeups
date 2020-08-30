900c1db462f55966d21b17e6e43394fd1263d4ca
Dmitriy Rodionov <57829161+Burgos1337@users.noreply.github.com>
2020-08-30 14:45:19 +0300

    some writeups added

--- 2020/SPRUSH CTF 2020/$eva flexx/solution/get_flag.py
+++ 2020/SPRUSH CTF 2020/$eva flexx/solution/get_flag.py
@@ -8,13 +8,13 @@ s = requests.Session()
 req = s.get("http://tasks.sprush.rocks:8084/")
 
 for k in range(150):
-   text = ""
+    text = ""
 
-   for symbol in req.text[666:]:
-     if (symbol != '$'):
-       text += symbol
-     else:
-       break
+    for symbol in req.text[666:]:
+        if (symbol != '$'):
+           text += symbol
+        else:
+           break
 
    print(text)
    text = str(process_sympy(text))
@@ -37,13 +37,13 @@ for k in range(150):
    fp.close()
    os.system('python seva.py > seva.txt')
    with open('seva.txt') as seva:
-      head = islice(seva, 1)
-      line = str(list(head))
-      answer = line[2 : len(line) - 4]
+        head = islice(seva, 1)
+        line = str(list(head))
+        answer = line[2 : len(line) - 4]
    print(answer)
 
    req = s.post("http://tasks.sprush.rocks:8084/", data = {'answer': answer})
    if ("SPR" not in req.text):
-     print(req.text[571:574])
+      print(req.text[571:574])
    else:
-     print(req.text)
+      print(req.text)
