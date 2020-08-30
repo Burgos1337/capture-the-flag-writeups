3c01d5f763dd9c41c6027cdf059b412db47c6957
Dmitriy Rodionov <57829161+Burgos1337@users.noreply.github.com>
2020-08-30 14:07:01 +0300

    some writeups added

--- 2020/DawgCTF 2020/Artur Ashe/get_flag.py
+++ 2020/DawgCTF 2020/Artur Ashe/get_flag.py
@@ -9,47 +9,47 @@ r.sendline('\n')
 flag = ""
 
 for i in range(700):
-        ahi = r.recv(8192).strip()
-        ahu = ahi.split(' ')
-        payload = ahu[-1][:-1]
+    ahi = r.recv(8192).strip()
+    ahu = ahi.split(' ')
+    payload = ahu[-1][:-1]
 
-        if '-love' in payload:
-                r.sendline('0')
-                flag += "0"
-                print(flag)
-        elif 'love-' in payload:
-                r.sendline('1')
-                flag += "1"
-                print(flag)
-        elif 'set-' in payload:
-                r.sendline('0')
-                flag += "0"
-                print(flag)
-        elif '-set' in payload:
-                r.sendline('1')
-                flag += "1"
-                print(flag)
-        elif 'game-' in payload:
-                r.sendline('0')
-                flag += "0"
-                print(flag)
-        elif '-game' in payload:
-                r.sendline('1')
-                flag += "1"
-                print(flag)
-        else:
-                pay = payload.split('-')
-                try:
-                  math = int(pay[0]) - int(pay[1])
-                  if math < 0:
-                        r.sendline('1')
-                        flag += "0"
-                        print(flag)
-                  else:
-                        r.sendline('0')
-                        flag += "1"
-                        print(flag)
-                except:
-                  pass
+    if '-love' in payload:
+       r.sendline('0')
+       flag += "0"
+       print(flag)
+    elif 'love-' in payload:
+       r.sendline('1')
+       flag += "1"
+       print(flag)
+    elif 'set-' in payload:
+       r.sendline('0')
+       flag += "0"
+       print(flag)
+    elif '-set' in payload:
+       r.sendline('1')
+       flag += "1"
+       print(flag)
+    elif 'game-' in payload:
+       r.sendline('0')
+       flag += "0"
+       print(flag)
+    elif '-game' in payload:
+       r.sendline('1')
+       flag += "1"
+       print(flag)
+    else:
+       pay = payload.split('-')
+       try:
+           math = int(pay[0]) - int(pay[1])
+           if math < 0:
+              r.sendline('1')
+              flag += "0"
+              print(flag)
+           else:
+              r.sendline('0')
+              flag += "1"
+              print(flag)
+       except:
+           pass
 
 # Then decode the binary string and get the flag
