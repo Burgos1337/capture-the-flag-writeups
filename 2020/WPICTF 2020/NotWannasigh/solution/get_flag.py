fa0fedb11ba6ae834942b0db03facfe30734cf62
Dmitriy Rodionov <57829161+Burgos1337@users.noreply.github.com>
2020-08-30 15:03:18 +0300

    some writeups added

--- 2020/WPICTF 2020/NotWannasigh/solution/get_flag.py
+++ 2020/WPICTF 2020/NotWannasigh/solution/get_flag.py
@@ -1,8 +1,8 @@
 with open('flag-gif.EnCiPhErEd', 'rb') as f:
-    x = [i for i in f.read()]
+     x = [i for i in f.read()]
 
 with open('key.txt', 'rb') as f:
-    y = list(map(int, f.read().split()))
+     y = list(map(int, f.read().split()))
 
 gif = []
 
@@ -10,4 +10,4 @@ for a, b in zip(x, y):
     gif.append(a ^ b)
 
 with open('flag.gif', 'wb') as f:
-    f.write(bytearray(gif))
+     f.write(bytearray(gif))
