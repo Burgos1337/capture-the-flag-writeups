7aa229fc9e07e55eaa4dbd0035e073574d3eebd0
Dmitriy Rodionov <57829161+Burgos1337@users.noreply.github.com>
2020-08-30 15:02:54 +0300

    some writeups added

--- 2020/WPICTF 2020/NotWannasigh/solution/get_key.cpp
+++ 2020/WPICTF 2020/NotWannasigh/solution/get_key.cpp
@@ -4,18 +4,17 @@
 
 FILE *f;
 
-int main()
-{
-   int i, n;
+int main() {
+    int i, n;
    
-   n = 374109;
+    n = 374109;
    
-   srand(1585599106); 
+    srand(1585599106); 
 
-   f = fopen("key.txt", "w");
-   for( i = 0 ; i < n ; i++ ) {
-    	fprintf(f, "%d", rand() & 0xff);
-   }
-   fclose(f);
-   return(0);
+    f = fopen("key.txt", "w");
+    for ( i = 0 ; i < n ; i++ ) {
+    	  fprintf(f, "%d", rand() & 0xff);
+    }
+    fclose(f);
+    return(0);
 }
