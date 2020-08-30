5165eb8e4e15654dddc0914b93bfff0a5b8e6695
Dmitriy Rodionov <57829161+Burgos1337@users.noreply.github.com>
2020-08-30 13:56:59 +0300

    CyberSchool MSU 2020 writeups added

--- 2020/CyberSchool MSU 2020/broadcast/broadcast.py
+++ 2020/CyberSchool MSU 2020/broadcast/broadcast.py
@@ -5,6 +5,6 @@ from secret import FLAG
 
 for i in range(1, 6):
     with open('{}.pub.pem'.format(i), 'rb') as f:
-        key = RSA.importKey(f.read())
+         key = RSA.importKey(f.read())
     with open('{}.enc'.format(i), 'wb') as f:
-        f.write(key.encrypt(FLAG.encode(), None)[0])
+         f.write(key.encrypt(FLAG.encode(), None)[0])
