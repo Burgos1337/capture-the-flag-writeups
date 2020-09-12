3902216d90d8da0917e85393515894184b998da3
Dmitriy Rodionov <57829161+Burgos1337@users.noreply.github.com>
2020-09-12 23:35:32 +0300

    some writeups added

--- 2020/ångstromCTF 2020/Shifter/solution.py
+++ 2020/ångstromCTF 2020/Shifter/solution.py
@@ -6,7 +6,7 @@ fib = lambda n:reduce(lambda x, n:[x[1], x[0] + x[1]], range(n), [0, 1])[0]
 def debug(txt, res, n):
     log.info("Text {txt} rotated with key {n}  --> {res}")
 
-def encrypt(text,s):
+def encrypt(text, s):
     result = ""
     for i in range(len(text)):
         result += chr((ord(text[i]) + s - 65) % 26 + 65)
