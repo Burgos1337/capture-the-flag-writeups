b36fd75591da5f6af1130444ff3afbaafb328692
Dmitriy Rodionov <57829161+Burgos1337@users.noreply.github.com>
2020-09-12 23:34:22 +0300

    some writeups added

--- 2020/ångstromCTF 2020/Secret Agents/app.py
+++ 2020/ångstromCTF 2020/Secret Agents/app.py
@@ -5,18 +5,18 @@ from .secret import host, user, passwd, dbname
 import mysql.connector
 
 dbconfig = {
-    "host":host,
-    "user":user,
-    "passwd":passwd,
-    "database":dbname
+    "host": host,
+    "user": user,
+    "passwd": passwd,
+    "database": dbname
 }
 
 app = Flask(__name__)
 """
 limiter = Limiter(
     app,
-    key_func=get_remote_address,
-    default_limits=["1 per second"],
+    key_func = get_remote_address,
+    default_limits = ["1 per second"],
 )"""
 
 #@limiter.exempt
@@ -31,7 +31,7 @@ def login():
     conn = mysql.connector.connect(**dbconfig)
     cursor = conn.cursor()
 
-    for r in cursor.execute("SELECT * FROM Agents WHERE UA='%s'"%(u), multi=True):
+    for r in cursor.execute("SELECT * FROM Agents WHERE UA = '%s'" % (u), multi = True):
 	if r.with_rows:
      	   res = r.fetchall()
 	   break
@@ -40,12 +40,12 @@ def login():
     conn.close()
 	
     if len(res) == 0:
-       return render_template("login.html", msg="stop! you're not allowed in here >:)")
+       return render_template("login.html", msg = "stop! you're not allowed in here >:)")
 
     if len(res) > 1:
-       return render_template("login.html", msg="hey! close, but no bananananananananana!!!! (there are many secret agents of course)")
+       return render_template("login.html", msg = "hey! close, but no bananananananananana!!!! (there are many secret agents of course)")
 
-    return render_template("login.html", msg="Welcome, %s"%(res[0][0]))
+    return render_template("login.html", msg = "Welcome, %s" % (res[0][0]))
 
 if __name__ == '__main__':
    app.run('0.0.0.0')
